76a37d5970089fcab41e0f855243ef58259ded08
MaiVanDang <dangmaivan24@gmail.com>
2025-07-23 15:04:56 +0700

    Practice on Bean Scope & Bean Lifecycle

--- maivandang/src/main/java/com/example/AppConfig.java
+++ maivandang/src/main/java/com/example/AppConfig.java
@@ -7,21 +7,21 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-//@ComponentScan(basePackages = "com.example")
+@ComponentScan(basePackages = "com.example")
 public class AppConfig {
 
-    @Bean
-    public Engine engine() {
-        return new Engine();
-    }
-
-    @Bean
-    public Car car() {
-//        return new Car(engine()); // constructor inject
-        //Setter inject
-        Car car = new Car();
-        car.setEngine(engine());
-        return car;
-    }
+//    @Bean
+//    public Engine engine() {
+//        return new Engine();
+//    }
+//
+//    @Bean
+//    public Car car() {
+////        return new Car(engine()); // constructor inject
+//        //Setter inject
+//        Car car = new Car();
+//        car.setEngine(engine());
+//        return car;
+//    }
 }
 
